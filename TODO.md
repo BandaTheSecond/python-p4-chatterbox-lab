05c70de3a1b70db10ffd234d0382dd9db34f10d6
Kingbanda <bandadesignske@gmail.com>
2025-09-27 09:34:29 +0300

    chatterbox lab completed

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# TODO for CRUD Messages Routes
+
+- [x] Implement GET /messages: Query all messages ordered by created_at ascending, return as JSON array
+- [x] Implement POST /messages: Create new message from body and username params, return new message as JSON
+- [x] Implement PATCH /messages/<int:id>: Update message body from params, return updated message as JSON
+- [x] Implement DELETE /messages/<int:id>: Delete message from database
--- server/app.py
+++ server/app.py
@@ -14,13 +14,30 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at).all()
+        return jsonify([msg.to_dict() for msg in messages])
+    elif request.method == 'POST':
+        data = request.get_json()
+        new_msg = Message(body=data['body'], username=data['username'])
+        db.session.add(new_msg)
+        db.session.commit()
+        return jsonify(new_msg.to_dict()), 201
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    msg = db.get_or_404(Message, id)
+    if request.method == 'PATCH':
+        data = request.get_json()
+        msg.body = data['body']
+        db.session.commit()
+        return jsonify(msg.to_dict())
+    elif request.method == 'DELETE':
+        db.session.delete(msg)
+        db.session.commit()
+        return '', 204
 
 if __name__ == '__main__':
     app.run(port=5555)
